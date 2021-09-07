252f43b005c8513966e808afb91661f9048788d5
Andrey Skopintsev <sanchezzvilapandozz@gmail.com>
2021-09-07 17:59:43 +0300

    added some functionality to single book buttons

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const monthNames = ["January", "February", "March", "April", "May", "June",
   "July", "August", "September", "October", "November", "December"
 ];
+const books = [{title:"Game of thrones",author:"George R. R. Martin",pages:"694",language:"English",published:"July 1 1996",status:"read"}]
 const formContainer = document.getElementById('formContainer')
 const newBook = document.getElementById('newBook')
 const closeForm = document.getElementById('closeForm')
@@ -27,6 +28,53 @@ function Book(title,author,pages,language,published,status){
     this.status = status
 }
 
+//Rendering books
+function renderBooks(){
+    booksContainer.innerHTML = ''
+    books.forEach(book => {
+        let div = document.createElement('div')
+        div.classList.add('single-book')
+        if(book.status == 'read'){
+            div.classList.add('read')
+        }
+        div.innerHTML = book.status == 'read' ? `
+        <button><i class="fas fa-times"></i></button>
+        <h2>${book.title}</h2>
+        <p>By: ${book.author}</p>
+        <p>Number of pages: ${book.pages}</p>
+        <p>Language: ${book.language}</p>
+        <p>Published: ${book.published}</p>
+        <input checked type="checkbox" id="readStatus">
+        `
+        :
+        `
+        <button><i class="fas fa-times"></i></button>
+        <h2>${book.title}</h2>
+        <p>By: ${book.author}</p>
+        <p>Number of pages: ${book.pages}</p>
+        <p>Language: ${book.language}</p>
+        <p>Published: ${book.date}</p>
+        <input type="checkbox" id="readStatus">
+        `
+        booksContainer.appendChild(div)
+        singleBookBtns(div)
+    })
+}
+
+//Hooking up single book's buttons
+function singleBookBtns(div){
+    console.log(div.querySelector('#readStatus'))
+    div.querySelector('#readStatus').addEventListener('change',()=>{
+        console.log(div.classList)
+        div.classList.toggle('read')
+    })
+    div.querySelector('button').addEventListener('click',()=>{
+        console.log('deleted')
+    })
+} 
+
+renderBooks()
+
 // Event listeners
 formContainer.addEventListener('submit',(e)=>{
     e.preventDefault()
@@ -40,32 +88,8 @@ formContainer.addEventListener('submit',(e)=>{
                         bookLanguage.value,
                         fullDate,
                         bookStatus.value)
-    console.log(book.published)
-    let div = document.createElement('div')
-    div.classList.add('single-book')
-    if(book.status == 'read'){
-        div.classList.add('read')
-    }
-    div.innerHTML = book.status == 'read' ? `
-        <button><i class="fas fa-times"></i></button>
-        <h2>${book.title}</h2>
-        <p>By: ${book.author}</p>
-        <p>Number of pages: ${book.pages}</p>
-        <p>Language: ${book.language}</p>
-        <p>Published: ${book.published}</p>
-        <input checked type="checkbox" id="readStatus">
-    `
-    :
-    `
-        <button><i class="fas fa-times"></i></button>
-        <h2>${book.title}</h2>
-        <p>By: ${book.author}</p>
-        <p>Number of pages: ${book.pages}</p>
-        <p>Language: ${book.language}</p>
-        <p>Published: ${book.date}</p>
-        <input type="checkbox" id="readStatus">
-    `
-    booksContainer.appendChild(div)
+    books.push(book)
+    renderBooks()
     formContainer.classList.remove('visible')
     bookTitle.value = ''
     bookAuthor.value = ''
